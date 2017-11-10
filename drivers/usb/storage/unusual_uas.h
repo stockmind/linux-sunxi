1710b898b82a5c96daef76b18973a4c545daa00e
Hans de Goede <hdegoede@redhat.com>
2017-11-10 16:07:51 +0100

    uas: Add US_FL_NO_ATA_1X quirk for one more Seagate device
    Just like all previous UAS capable Seagate disk enclosures, this
    one needs a US_FL_NO_ATA_1X quirk.
    
    Cc: stable@vger.kernel.org # 3.16
    Signed-off-by: Andrey Astafyev <1@246060.ru>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/usb/storage/unusual_uas.h
+++ drivers/usb/storage/unusual_uas.h
@@ -108,6 +108,13 @@ UNUSUAL_DEV(0x0bc2, 0xab2a, 0x0000, 0x9999,
 		USB_SC_DEVICE, USB_PR_DEVICE, NULL,
 		US_FL_NO_ATA_1X),
 
+/* Reported-by: Andrey Astafyev <dev@246060.ru> */
+UNUSUAL_DEV(0x0bc2, 0xab30, 0x0000, 0x9999,
+		"Seagate",
+		"BUP BK",
+		USB_SC_DEVICE, USB_PR_DEVICE, NULL,
+		US_FL_NO_ATA_1X),
+
 /* Reported-by: Benjamin Tissoires <benjamin.tissoires@redhat.com> */
 UNUSUAL_DEV(0x13fd, 0x3940, 0x0000, 0x9999,
 		"Initio Corporation",
